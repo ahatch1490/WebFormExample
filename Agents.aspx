de6d24ad372b03a951835719043eea51815dd457
Anthony Hatch <ahatch1490@gmail.com>
2018-09-24 08:19:29 -0700

    Adding files to repo

---
+++ Agents.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Agents.aspx.cs" Inherits="WebFormExample.Agents" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView runat="server" CssClass="table-striped" ID="gvAgent" >
+    </asp:GridView>
+</asp:Content>
---
+++ Agents.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration; 
+using System.Threading.Tasks;
+using MoxiWorks.Platform; 
+namespace WebFormExample
+{
+    public partial class Agents : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var companyId = System.Configuration.ConfigurationManager.AppSettings["CompanyId"]; 
+            var client = new MoxiWorksClient();
+            var service = new AgentService(client);
+            var source = System.Threading.Tasks.Task.Run(() => service.GetAgentsAsync(companyId, null, DateTime.Now.AddDays(-100))).Result; 
+            gvAgent.DataSource = source.Item.Agents; 
+            gvAgent.DataBind();
+        }
+    }
+}
